795a6456236e54beda2da8b2c7272ba3500923aa
kundan Kumar <kkundan501@gmail.com>
2023-06-03 10:58:20 +0530

    V1.03

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -7,7 +7,7 @@ import { MdOutgoingMail } from "react-icons/md";
 export default function Footer() {
     return (
         <footer>
-        <p className='footer-title'>For Any queries </p>
+        <p className='footer-title'>Any questions or remarks?</p>
             <div className="footer-link">
                 <Link to="https://kundnkr.netlify.app/#contact">Contact Us<FaShareSquare className='logo footer-icon' size={"25px"} /></Link>
                 <Link to="mailto:Mrkundan19@gmail.com">Write a Mail<MdOutgoingMail className='logo footer-icon' size={"25px"} /></Link>
