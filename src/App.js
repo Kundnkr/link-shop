86de70e346f9155ff83e69956d67f23ae8be9f2f
kundan kumar <mrkundnan19@gmail.com>
2023-06-30 11:17:54 +0530

    1.05

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import General from './Component/General';
 import Footer from './Component/Footer';
 import { BrowserRouter } from 'react-router-dom'
-import { FaBriefcase, FaCoffee, FaGithub, FaInstagram, FaYoutube } from "react-icons/fa";
+import { FaBriefcase, FaCoffee, FaGithub, FaInstagram, FaYoutube, FaDiscord, FaTelegram} from "react-icons/fa";
 
 
 
@@ -12,6 +12,16 @@ function App() {
     name: "Portfolio",
     disc: "Explore my portfolio website to discover a curated collection of my creative work, showcasing my skills in design, development, and more.",
     link: "https://kundankr.online/"
+  }
+  let discord = {
+    name: "Discord | Developer community",
+    disc: "Join our web developer community server on Discord, where you can ask any questions related to web development and help others with their queries. Let's come together to share knowledge and support each other in our web development journey.",
+    link: "https://discord.gg/KYfPpKbZmE"
+  }
+  let telegram = {
+    name: "Telegram | Developer community",
+    disc: "Join our web developer community on Telegram, where you can ask any questions related to web development and help others with their queries. Let's come together to share knowledge and support each other in our web development journey.",
+    link: "https://t.me/+gnB75XnlxpUxOTRl"
   }
   let BuyCoffee = {
     name: "Buy Me a Coffee",
@@ -43,11 +53,13 @@ function App() {
           <div className="heading" >
             <li>General</li>
           </div>
-          <General delay = "5000" className = "card" name={portfolio.name} link={portfolio.link} disc={portfolio.disc} logo={<FaBriefcase className='logo' size={"30px"} />} />
-          <General delay = "5000" className = "card" name={youtube.name} link={youtube.link} disc={youtube.disc} logo={<FaYoutube className='logo' size={"30px"} />} />
-          <General delay = "5000" className = "card" name={BuyCoffee.name} link={BuyCoffee.link} disc={BuyCoffee.disc} logo={<FaCoffee className='logo' size={"30px"} />} />
-          <General delay = "5000" className = "card" name={gihub.name} link={gihub.link} disc={gihub.disc} logo={<FaGithub className='logo' size={"30px"} />} />
-          <General delay = "5000" className = "card" name={instagram.name} link={instagram.link} disc={instagram.disc} logo={<FaInstagram className='logo' size={"30px"} />} />
+          <General delay="5000" className="card" name={portfolio.name} link={portfolio.link} disc={portfolio.disc} logo={<FaBriefcase className='logo' size={"30px"} />} />
+          <General delay="5000" className="card" name={youtube.name} link={youtube.link} disc={youtube.disc} logo={<FaYoutube className='logo' size={"30px"} />} />
+          <General delay="5000" className="card" name={discord.name} link={discord.link} disc={discord.disc} logo={<FaDiscord className='logo' size={"30px"} />} />
+          <General delay="5000" className="card" name={telegram.name} link={telegram.link} disc={telegram.disc} logo={<FaTelegram className='logo' size={"30px"} />} />
+          <General delay="5000" className="card" name={BuyCoffee.name} link={BuyCoffee.link} disc={BuyCoffee.disc} logo={<FaCoffee className='logo' size={"30px"} />} />
+          <General delay="5000" className="card" name={gihub.name} link={gihub.link} disc={gihub.disc} logo={<FaGithub className='logo' size={"30px"} />} />
+          <General delay="5000" className="card" name={instagram.name} link={instagram.link} disc={instagram.disc} logo={<FaInstagram className='logo' size={"30px"} />} />
         </div>
         <Footer />
       </BrowserRouter>
